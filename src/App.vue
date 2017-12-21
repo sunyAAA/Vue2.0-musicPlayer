ef319fd0578f7e6d28a638a2dc48c5a73abae319
sunyAAA <xsannie15@gmail.com>
2017-12-21 16:26:29 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <div id="app">
-    hello world!!
+    <m-header></m-header>
   </div>
 </template>
 
 <script>
+import MHeader from './components/m-header/m-header'
 export default {
-  name: "app"
+  components:{
+    MHeader
+  }
 };
 </script>
 
-<style>
+<style lang="stylus" scoped>
 
 </style>
+
--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'babel-polyfill'
+import fastClick from 'fastclick'
+// 引入依赖
 import Vue from 'vue'
 import App from './App'
 //import router from './router'
 import 'common/stylus/index.styl'
-
+//fastclick 用法
+fastClick.attach(document.body)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */

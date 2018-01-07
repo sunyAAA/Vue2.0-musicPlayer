7265cff118300ebe614c3dc9cc4c77c23e4981ea
sunyAAA <xsannie15@gmail.com>
2018-01-07 18:05:58 +0800

    update

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,104 +1,148 @@
 <template>
   <div class="recommend">
-      <div class="recommend-content">
+    <scroll ref='scroll' class="recommend-content" :data='discList'>
+      <div>
         <div v-if='recommends.length' class="slider-wrapper">
           <slider>
             <div v-for='(item,index) in recommends' :key="index">
               <a :href="item.linkUrl">
-                <img :src="item.picUrl">
+                <img @load='loadImg' :src="item.picUrl">
               </a>
             </div>
           </slider>
         </div>
         <div class="recommend-list">
           <h1 class="list-title">热门歌单推荐</h1>
+          <ul>
+            <li v-for='(item,index) in discList' class="item" :key='index'>
+              <div class="icon">
+                <img v-lazy="item.picUrl" alt="" width="60px" height="60px">
+              </div>
+              <div class="text">
+                <h2 class="name" v-html='item.songListAuthor'></h2>
+                <p class="desc" v-html='item.songListDesc'></p>
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
+    </scroll>
   </div>
 </template>
 
 <script>
-import {getRecommend} from 'api/recommend'
-import {ERR_OK} from 'api/config'
-import Slider from 'base/slider/slider'
+import { getRecommend } from "api/recommend";
+import { ERR_OK } from "api/config";
+import Slider from "base/slider/slider";
+import Scroll from "base/scroll/scroll";
 
 export default {
-  data(){
-    return{
-      recommends:[]
-    }
+  data() {
+    return {
+      recommends: [],
+      discList: [],
+      checkLoaded:false
+    };
   },
-  components:{
-    Slider
+  components: {
+    Slider,
+    Scroll
   },
-  created(){
+  created() {
     this._getRecommend();
     this._getDiscList();
   },
-  methods:{
-    _getRecommend(){
-      getRecommend().then((res)=>{
-        if(res.code === ERR_OK){
+  methods: {
+    _getRecommend() {
+      getRecommend().then(res => {
+        if (res.code === ERR_OK) {
           this.recommends = res.data.slider;
         }
-      })
+      });
     },
-    _getDiscList(){
-      this.$http.get('api/getDiscList').then((res)=>{
-        console.log(res.data);
-      })
+    _getDiscList() {
+      this.$http.get("api/getDiscList").then(res => {
+        this.discList = res.data.data.songList;
+      });
+    },
+    loadImg(){
+      if(!this.checkLoaded){
+       this.$refs.scroll.refresh();
+      }else{
+        return
+      }
     }
   }
-}
+};
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-  @import "~common/stylus/variable"
+@import '~common/stylus/variable';
 
-  .recommend
-    position: fixed
-    width: 100%
-    top: 88px
-    bottom: 0
-    .recommend-content
-      height: 100%
-      overflow: hidden
-      .slider-wrapper
-        position: relative
-        width: 100%
-        overflow: hidden
-      .recommend-list
-        .list-title
-          height: 65px
-          line-height: 65px
-          text-align: center
-          font-size: $font-size-medium
-          color: $color-theme
-        .item
-          display: flex
-          box-sizing: border-box
-          align-items: center
-          padding: 0 20px 20px 20px
-          .icon
-            flex: 0 0 60px
-            width: 60px
-            padding-right: 20px
-          .text
-            display: flex
-            flex-direction: column
-            justify-content: center
-            flex: 1
-            line-height: 20px
-            overflow: hidden
-            font-size: $font-size-medium
-            .name
-              margin-bottom: 10px
-              color: $color-text
-            .desc
-              color: $color-text-d
-      .loading-container
-        position: absolute
-        width: 100%
-        top: 50%
-        transform: translateY(-50%)
+.recommend {
+  position: fixed;
+  width: 100%;
+  top: 88px;
+  bottom: 0;
+
+  .recommend-content {
+    height: 100%;
+    overflow: hidden;
+
+    .slider-wrapper {
+      position: relative;
+      width: 100%;
+      overflow: hidden;
+    }
+
+    .recommend-list {
+      .list-title {
+        height: 65px;
+        line-height: 65px;
+        text-align: center;
+        font-size: $font-size-medium;
+        color: $color-theme;
+      }
+
+      .item {
+        display: flex;
+        box-sizing: border-box;
+        align-items: center;
+        padding: 0 20px 20px 20px;
+
+        .icon {
+          flex: 0 0 60px;
+          width: 60px;
+          padding-right: 20px;
+        }
+
+        .text {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          flex: 1;
+          line-height: 20px;
+          overflow: hidden;
+          font-size: $font-size-medium;
+
+          .name {
+            margin-bottom: 10px;
+            color: $color-text;
+          }
+
+          .desc {
+            color: $color-text-d;
+          }
+        }
+      }
+    }
+
+    .loading-container {
+      position: absolute;
+      width: 100%;
+      top: 50%;
+      transform: translateY(-50%);
+    }
+  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Axios from 'axios'
+import VueLazyLoad from 'vue-lazyload'
 import 'common/stylus/index.styl'
 //fastclick 用法
 fastClick.attach(document.body)
@@ -14,6 +15,9 @@ Vue.config.productionTip = false
 //axios 注册
 Vue.prototype.$http = Axios;
 /* eslint-disable no-new */
+Vue.use(VueLazyLoad,{
+  loading: require('common/image/default.png')
+})
 new Vue({
   el: '#app',
   router,

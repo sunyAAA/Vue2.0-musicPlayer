67f63f6eda8ca7a5eb7a36053a524da2a687ff33
sunyAAA <xsannie15@gmail.com>
2018-01-08 21:02:41 +0800

    update

--- src/base/scroll/scroll.vue
+++ src/base/scroll/scroll.vue
@@ -50,7 +50,7 @@ export default {
     data(){
       setTimeout(() => {
         this.refresh();
-      }, 60);
+      }, 20);
     }
   }
 }
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="recommend">
-    <scroll ref='scroll' class="recommend-content" :data='discList'>
+    <scroll ref='scroll' class="recommend-content" :data='recommends'>
       <div>
         <div v-if='recommends.length' class="slider-wrapper">
           <slider>

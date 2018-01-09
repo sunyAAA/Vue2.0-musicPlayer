543b8c0ca56c36f0cc07672b83e98670c0659c65
sunyAAA <xsannie15@gmail.com>
2018-01-09 18:36:58 +0800

    update

--- src/base/scroll/scroll.vue
+++ src/base/scroll/scroll.vue
@@ -5,57 +5,56 @@
 </template>
 
 <script>
-import BScroll from 'better-scroll'
+import BScroll from "better-scroll";
 export default {
-  props:{
-    probeType:{
-      type:Number,
-      default:1
+  props: {
+    probeType: {
+      type: Number,
+      default: 1
     },
-    click:{
-      type:Boolean,
-      default:true
+    click: {
+      type: Boolean,
+      default: true
     },
-    data:{
-      type:Array,
-      default:null
+    data: {
+      type: Array,
+      default: null
     }
   },
-  methods:{
-    _initScroll(){
-      if(!this.$refs.wrapper){
-        return
+  methods: {
+    _initScroll() {
+      if (!this.$refs.wrapper) {
+        return;
       }
-      this.scroll = new BScroll(this.$refs.wrapper,{
-        probeType:this.probeType,
-        click:this.click
-      })
+      this.scroll = new BScroll(this.$refs.wrapper, {
+        probeType: this.probeType,
+        click: this.click
+      });
     },
-    enable(){
+    enable() {
       this.scoll && this.scroll.enable();
     },
-    disable(){
+    disable() {
       this.scroll && this.scroll.disable();
     },
-    refresh(){
+    refresh() {
       this.scroll && this.scroll.refresh();
     }
   },
-  mounted(){
-    setTimeout(()=>{
-      this._initScroll()
-    },20)
+  mounted() {
+    setTimeout(() => {
+      this._initScroll();
+    }, 20);
   },
-  watch:{
-    data(){
+  watch: {
+    data() {
       setTimeout(() => {
         this.refresh();
       }, 20);
     }
   }
-}
+};
 </script>
 
 <style lang="stylus" scoped>
-
 </style>

16dbbb38614fac8cf4b4b361895fb46c17602f7e
sunyAAA <xsannie15@gmail.com>
2018-01-11 16:54:03 +0800

    update

--- src/base/listview/listview.vue
+++ src/base/listview/listview.vue
@@ -20,14 +20,22 @@
         >{{item}}</li>
       </ul>
     </div>
+    <div class="list-fixed" v-show='fixedTitle' ref='fixed'>
+      <h1 class="fixed-title">{{fixedTitle}}</h1>
+    </div>
+    <div class='loading-container' v-show='!data.length'>
+      <loading></loading>
+    </div>
   </scroll>
 </template>
 
 <script>
 import Scroll from 'base/scroll/scroll'
 import {getData} from 'common/js/dom'
+import Loading from 'base/loading/loading'
 
 const ANCHOR_HEIGHT = 18
+const TITLE_HEIGHT = 30
 
 export default {
   created(){
@@ -39,7 +47,8 @@ export default {
   data(){
     return{
       scrollY : -1,
-      currentIndex : 0
+      currentIndex : 0,
+      diff:-1
     }
   },
   props:{
@@ -53,6 +62,10 @@ export default {
       return this.data.map((gropu)=>{
         return gropu.title.substr(0,1)
       })
+    },
+    fixedTitle(){
+      if(this.scrollY>0) return '';
+      return this.data[this.currentIndex] ? this.data[this.currentIndex].title : ''
     }
   },
   methods:{
@@ -118,14 +131,23 @@ export default {
         let height2 = listHeight[i+1];
         if( (-newY>=height1 && -newY <= height2)){
           this.currentIndex = i 
+          this.diff = height2 + newY
           return
         }
       }
       // 当滚动到底部 且 -newY > 最后一个元素的上限
       this.currentIndex = this.listHeight -2
+    },
+    diff(newVal){
+      let fixedTop = (newVal>0 && newVal<TITLE_HEIGHT)? newVal - TITLE_HEIGHT : 0
+      if(this.fixedTop === fixedTop){
+        return
+      }
+      this.fixedTop = fixedTop
+      this.$refs.fixed.style.transform  = `translate3d(0,${fixedTop}px,0)`
     }
   },
-  components:{ Scroll}
+  components:{ Scroll,Loading}
 }
 </script>
 
